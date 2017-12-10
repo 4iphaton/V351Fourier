8d81f5bb199b3d0161f095887beaa001483c15fe
Jan Ellbracht <jan.ellbracht@tu-dortmund.de>
2017-12-10 22:01:58 +0100

    1.2.6

---
+++ content/python/b_recht.py
@@ -0,0 +1,15 @@
+import numpy as np
+import scipy.constants as const
+from uncertainties import ufloat
+from uncertainties import unumpy as unp
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import scipy.constants as const
+import math
+
+U, v = np.genfromtxt('../../content/values/b_rechteck.txt',unpack=True)
+k = 0
+for x in U:
+    k = 4/(const.pi*(x + 1))
+    print(k)
+    print((k-U)/k)
